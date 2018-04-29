8e2015f8ba94b071babd35e4c9540d3da1edde0e
Claudio Figueiredo <cfigueiredo@shutterstock.com>
2018-04-28 22:58:07 -0400

    adds extra test for lists syntax on for loops

--- tests/compiling/test_for_loops.py
+++ tests/compiling/test_for_loops.py
@@ -5,6 +5,28 @@ from tests.helpers import get_representation, parse
 
 # noinspection PyMethodMayBeStatic
 class TestForLoopSyntax:
+    def test_is_supported_with_variables(self):
+        expr = """
+        for item in list
+            print(item)
+        end
+        """
+
+        repres = get_representation(expr)
+        repres.should.contain('for_ var item var list')
+        repres.should.contain('block print var item')
+
+    def test_is_supported_with_list(self):
+        expr = """
+        for item in [1, 2, 3]
+            print(item)
+        end
+        """
+
+        repres = get_representation(expr)
+        repres.should.contain('for_ var item list int 1 int 2 int 3')
+        repres.should.contain('block print var item')
+
     def test_is_supported(self):
         expr = """
         for item in list
