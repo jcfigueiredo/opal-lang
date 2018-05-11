9688ec62007352bfba8638bba14df2352795a197
Claudio Figueiredo <cfigueiredo@shutterstock.com>
2018-05-10 22:06:42 -0400

    creates ir for classes

--- tests/compiling/test_base_type.py
+++ tests/compiling/test_base_type.py
@@ -1,7 +1,5 @@
-from wurlitzer import pipes
-
-
 # noinspection PyMethodMayBeStatic
+from opal.codegen import CodeGenerator
 from opal.evaluator import OpalEvaluator
 from tests.helpers import get_representation, parse
 
@@ -15,6 +13,17 @@ class TestTypeCreationSyntax:
 
         repres = get_representation(expr)
         repres.should.contain('program block class_ name Object block')
+
+
+# class TestTypeInheritanceSyntax:
+#     def test_is_supported_with_variables(self):
+#         expr = """
+#         class Integer < Object
+#         end
+#         """
+#
+#         repres = get_representation(expr)
+#         repres.should.contain('program block class_ name Integer < name Object block')
 
 
 class TestTypeCreationAST:
@@ -41,8 +50,14 @@ class TestCreatingANewType:
     def test_has_the_right_struct(self):
         self.code.should.contain('%"Object" = type {}')
 
-    def test_creates_a_vtable(self):
-        self.code.should.contain('%"vtable_Object_type" = type {%"vtable_Object_type"*, i8*}')
+    def test_creates_the_vtable_type(self):
+        self.code.should.contain('%"Object_vtable_type" = type {%"Object_vtable_type"*, i8*}')
 
     def test_sets_the_type_name(self):
         self.code.should.contain('private unnamed_addr constant [7 x i8] c"Object\\00"')
+
+    def test_creates_the_vtable(self):
+        class_name = CodeGenerator.get_string_name('Object')
+        self.code.should.contain(
+            f'@"Object_vtable" = private constant %"Object_vtable_type" {{%"Object_vtable_type"* null, '
+            f'i8* getelementptr ([7 x i8], [7 x i8]* @"{class_name}", i32 0, i32 0)}}')
